f751efac035fc90c2484ced8b36c73f59640a7bf
Kristen GARNIER <kgarnier@iris-interactive.fr>
2018-06-14 23:49:05 +0200

    Add database actions

--- pkg/models/database.go
+++ pkg/models/database.go
@@ -27,3 +27,50 @@ func (db *Database) GetSnippet(id int) (*Snippet, error) {
 
 	return s, nil
 }
+
+func (db *Database) GetLatestSnippets() ([]*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+
+	rows, err := db.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
+}
+
+func (db *Database) InsertSnippet(title, content, expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? SECOND))`
+
+	result, err := db.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
