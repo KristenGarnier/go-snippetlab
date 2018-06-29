d39b957310fadc93d9008a03b4b19fedc20a428c
Kristen GARNIER <kgarnier@iris-interactive.fr>
2018-06-30 00:13:24 +0200

    Add url path and buffer for no partial render error

--- cmd/web/views.go
+++ cmd/web/views.go
@@ -1,32 +1,54 @@
 package main
 
 import (
+	"bytes"
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"snippetbox.org/pkg/models"
 )
 
 type HTMLData struct {
-	Snippet *models.Snippet
+	Path     string
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
 
-func (app *App) RenderHTML(w http.ResponseWriter, page string, data *HTMLData) {
+func (app *App) RenderHTML(w http.ResponseWriter, r *http.Request, page string, data *HTMLData) {
+	if data == nil {
+		data = &HTMLData{}
+	}
+
+	data.Path = r.URL.Path
+
 	files := []string{
 		filepath.Join(app.HTMLDir, "base.html"),
 		filepath.Join(app.HTMLDir, page),
 	}
 
-	ts, err := template.ParseFiles(files...)
+	fm := template.FuncMap{
+		"humanDate": humanDate,
+	}
+
+	ts, err := template.New("").Funcs(fm).ParseFiles(files...)
 	if err != nil {
 		app.ServerError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", data)
+	buf := new(bytes.Buffer)
+
+	err = ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.ServerError(w, err)
 		return
 	}
+
+	buf.WriteTo(w)
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
 }
